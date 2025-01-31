f696225be699b031a77ba17baaeacf287985eabf
SabaMaghradze <sabamaghradze1001@gmail.com>
2025-01-31 17:22:34 +0400

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react'
 import './App.css'
-import AddRoom from './components/room/AddRoom'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import AddRoom from './components/room/AddRoom';
+import EditRoom from './components/room/EditRoom';
 import ExistingRooms from './components/room/ExistingRooms';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <AddRoom/>
-      <ExistingRooms/>
+      <main>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/edit-room/:roomId' element={<EditRoom />} />
+            <Route path='/existing-rooms' element={<ExistingRooms />} />            
+          </Routes>
+        </Router>
+
+      </main>
     </>
   )
 }
