8fb8523eae2d5bf6c4b353b9bc974bc1d2e3e8ea
SabaMaghradze <sabamaghradze1001@gmail.com>
2025-02-06 12:37:03 +0400

    Homepage adorning: MainHeader, Header, Parallax, HotelSErvice

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import MainHeader from '../layout/MainHeader'
+import HotelService from '../common/HotelService'
+import Parallax from '../common/Parallax'
 
 const Home = () => {
   return (
-    <div>Welcome to the homepage</div>
+    <section>
+      <MainHeader />
+      <section className='container'>
+        <Parallax />
+        <HotelService />
+        <Parallax />
+      </section>
+    </section>
   )
 }
 
